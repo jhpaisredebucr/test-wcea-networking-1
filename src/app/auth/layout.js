da57e5734caca3817d8bdbaf927962aa276f2d58
Paulo <pauloreeve143@gmail.com>
2026-03-18 09:57:30 +0800

    final

--- src/app/auth/layout.js
+++ src/app/auth/layout.js
@@ -3,7 +3,7 @@ import "../globals.css";
 export default function RootLayout({ children }) {
   return (
     <div className="w-screen h-screen flex justify-center items-center">
-      <div className="w-full max-w-md p-10 bg-[#ffffff] shadow-xl rounded-xl">
+      <div className="w-full max-w-sm">
       {children}
       </div>
     </div>
