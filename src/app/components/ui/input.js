58a2fc978ed7636ca5d847e39b76573c46af4520
jhoremzisaacsanpedrodelacruz <jhoremzisaacdelacruz@gmail.com>
2026-03-18 09:07:38 +0800

    feat: changed sign forms component

--- src/app/components/ui/input.js
+++ src/app/components/ui/input.js
@@ -1,13 +1,14 @@
-export default function Input({label, type="text", value, onChange}) {
+export default function Input({ label, type = "text", value, onChange }) {
     return (
-        <div>
-            <p className="text-sm">{label}</p>
+        <div className="mb-4">
+            <p className="text-sm text-gray-500">{label}</p>
             <input
                 type={type}
                 value={value}
                 onChange={(e) => onChange(e.target.value)}
-                className="w-full  border border-gray-300 mb-2 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                placeholder={label} // placeholder shows the label text
+                className="w-full border border-gray-300 rounded-md p-2 mb-2 text-gray-700 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-500"
             />
         </div>
-    )
+    );
 }
