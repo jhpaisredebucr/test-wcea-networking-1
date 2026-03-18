c42f69e0690eee2b9734b48d4b8e8461f082153b
Paulo <pauloreeve143@gmail.com>
2026-03-18 09:16:35 +0800

    revert

--- src/app/api/signup/route.js
+++ src/app/api/signup/route.js
@@ -1,7 +1,5 @@
 import { query } from "../../../../lib/db";
 import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
-import { cookies } from "next/headers";
 
 export async function POST(req) {
     try {
@@ -40,18 +38,6 @@ export async function POST(req) {
         );
 
         const user = result[0];
-
-        const token = jwt.sign(
-            { id: user.id, username: user.username, role: user.role },
-            process.env.JWT_SECRET,
-            { expiresIn: "1d" }
-        );
-
-        cookies().set("token", token, {
-            httpOnly: true,
-            secure: true,
-            path: "/",
-        });
 
         return Response.json({
             success: true,
