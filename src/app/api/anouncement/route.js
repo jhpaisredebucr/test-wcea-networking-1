ec4ebfdc14ef067887c2eb4d177220459143ec3f
Paulo <pauloreeve143@gmail.com>
2026-03-24 21:34:16 +0800

    fix: announcement route query

--- src/app/api/anouncement/route.js
+++ src/app/api/anouncement/route.js
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import { query } from "../../../../lib/db";
 
 export async function GET(req) {
-    const announcements = await query("SELECT * FROM anouncement"); 
+    const announcements = await query("SELECT * FROM announcement"); 
 
     return NextResponse.json({announcements});
 }
