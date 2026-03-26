ed69b40a7338b85204be8511a674adaab3eff1ca
Paulo <pauloreeve143@gmail.com>
2026-03-26 21:50:48 +0800

    test for topbar error

--- src/app/u/cmpnts/pages/referrals.js
+++ src/app/u/cmpnts/pages/referrals.js
@@ -1,7 +1,29 @@
+import Card from "../common/card";
+
 export default function ReferralsMember({userInfo}) {
     return (
         <div>
             <p>Your Referral Code: {userInfo?.referral_code}</p>
+
+            <div className="grid grid-cols-2 gap-5 mt-5">
+                <Card title="Total Referred" value="0" info=""/>
+                <Card title="Pending" value="0" info=""/>
+            </div>
+
+            <div className="grid grid-cols-4 shadow-sm p-5 mt-5 rounded-lg bg-white">
+                <div>
+                    <p>Username</p> 
+                </div>
+                <div>
+                    <p>Full Name</p>
+                </div>
+                <div>
+                    <p>Data Joined</p>
+                </div>
+                <div>
+                    <p>Status</p>
+                </div>
+            </div>
         </div>
     )
 }
