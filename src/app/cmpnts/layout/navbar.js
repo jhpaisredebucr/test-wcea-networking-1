11ceaff165f5f5b97030f1038a2ec27ae28e8caf
jhoremzisaacsanpedrodelacruz <jhoremzisaacdelacruz@gmail.com>
2026-03-26 19:03:56 +0800

    refactor: layouts + edited cdd

--- src/app/cmpnts/layout/navbar.js
+++ src/app/cmpnts/layout/navbar.js
@@ -1,5 +1,6 @@
 "use client"
 import { useRouter } from "next/navigation"
+import Image from "next/image";
 
 export default function Navbar() {
     const router = useRouter();
@@ -21,13 +22,14 @@ export default function Navbar() {
     }
 
     return (
-        <nav className="flex w-screen border-b-2 border-(--foreground)/50 border- h-14 items-center justify-between px-5 ">
+        <nav className="flex 0 w-screen border-b-2 border-(--foreground)/50 border- h-14 items-center justify-between px-5 ">
+            <img src="/images/logo.ico" alt="logo" className="w-10"/>
             <h1 className="text-lg">Health and Wellness Hatdog</h1>
-            <div>
-                <button onClick={Home} className="mx-2 p-2 rounded-2xl hover:bg-(--background) hover:text-(--foreground)">Home</button>
-                <button onClick={About} className="mx-2 p-2 rounded-2xl hover:bg-gray-100">About Us</button>
-                <button onClick={Register} className="mx-2 p-2 rounded-2xl hover:bg-gray-100">Register</button>
-                <button onClick={Contacts} className="mx-2 p-2 rounded-2xl hover:bg-gray-100">Contacts</button>
+            <div className="transition duration-50">
+                <button onClick={Home} className="mx-2 p-2 rounded-2xl transition duration-50 hover:bg-(--background) hover:text-(--foreground)">Home</button>
+                <button onClick={About} className="mx-2 p-2 rounded-2xl transition duration-100 hover:bg-gray-100">About Us</button>
+                <button onClick={Register} className="mx-2 p-2 rounded-2xl  transition duration-100 hover:bg-gray-100">Register</button>
+                <button onClick={Contacts} className="mx-2 p-2 rounded-2xl  transition duration-100 hover:bg-gray-100">Contacts</button>
             </div>
       </nav>
     )
--- src/app/home/layout.js
+++ src/app/home/layout.js
@@ -2,6 +2,11 @@ import "../globals.css";
 import Navbar from "@/app/cmpnts/layout/navbar";
 import TopBar from "../cmpnts/layout/top-bar";
 
+export const metadata = {
+  title: "WCEA Home",
+  description: "Dashboard for users to view their health and wellness data, track progress, and access personalized recommendations.",
+};
+
 export default function RootLayout({ children }) {
   return (
     <div>
