c54cc13645b23b0edc6f70e7ac912f1ae5eebc62
jhoremzisaacsanpedrodelacruz <jhoremzisaacdelacruz@gmail.com>
2026-03-26 15:19:19 +0800

    fix: deployment error

--- src/app/ui/page.js
+++ src/app/ui/page.js
@@ -3,6 +3,7 @@
 import Input from "@/app/cmpnts/ui/input"
 import { useState } from "react";
 import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 export default function TestUI(){
     const router = useRouter();
@@ -21,7 +22,6 @@ export default function TestUI(){
                 <div className="flex mt-4 mx-4 w-300  h-200">
                     <div onClick={(e)=>{
                         e.preventDefault();
-                        router.push("/test/ui/side-nav");
                     }} className="bg-gradient-to-t from-gray-500 to-gray-200 mx-2 hover:shadow-xl w-1/2 h-1/2 transform hover:scale-101 bg-gray-200 rounded-lg flex items-center opacity-50 duration-200 quad-in hover:opacity-100 justify-center hover:cursor-pointer hover:bg-blue-200 border-gray-400 border-transparent hover:border-gray-400">
                         <div className="border-1 rounded-full animate-ping w-8 h-8"></div>
                         <p className="text-gray-600">Side nav</p>
