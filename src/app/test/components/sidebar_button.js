2727b0a42fa8f371df39e16db45a7125bc7ec1bc
jhoremzisaacsanpedrodelacruz <jhoremzisaacdelacruz@gmail.com>
2026-03-26 10:55:58 +0800

    test feat: removed /test directory + moves ui testing to root directory

--- src/app/test/components/sidebar_button.js
+++
@@ -1,7 +0,0 @@
-export default function SidebarButton({ children }) {
-    return (
-        <button className="w-full text-left px-3 py-2 rounded-lg hover:bg-gray-100 transition">
-            {children}
-        </button>
-    )
-}
--- src/app/test/components/ui/error_text.js
+++
@@ -1,7 +0,0 @@
-export default function ErrorText({label}) {
-    return (
-        <div>
-            <p className="text-color-red">{label}</p>
-        </div>
-    )
-}
