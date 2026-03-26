b1f5245f8855dd64a5db2513fb248dd8d86dafeb
jhoremzisaacsanpedrodelacruz <jhoremzisaacdelacruz@gmail.com>
2026-03-26 08:46:51 +0800

    feat: updated input component css

--- src/app/components/ui/input.js
+++ src/app/components/ui/input.js
@@ -17,6 +17,7 @@ function Input({ label, type = "text", value, onChange, require, className = "" 
     //     ? "border-red-500 focus:ring-2 focus:ring-red-400" 
     //     : "border-gray-300 focus:ring-1 focus:ring-blue-400"}
     // ${className}
+
     return (
         
         <div className="mb-4">
@@ -28,10 +29,11 @@ function Input({ label, type = "text", value, onChange, require, className = "" 
                 onChange={(e) => onChange(e.target.value)}
                 // onBlur={() => setTouched(true)}
                 className={`
+                    border-gray-400 
                     w-full border rounded-md p-2 mb-2
                     text-gray-800 placeholder-gray-400
-                    focus:outline-none transition duration-100
-
+                    focus:outline-none transition duration-200
+                    focus:ring-1 focus:ring-blue-400 focus:border-blue-200
                 `}
             />
         </div>
