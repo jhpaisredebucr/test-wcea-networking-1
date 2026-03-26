74914c18bf5082d5c8cab9856c61ea4f3acdc142
Paulo <pauloreeve143@gmail.com>
2026-03-26 18:39:16 +0800

    feat: dashboard member content, no database yet

--- src/app/u/cmpnts/common/card.js
+++ src/app/u/cmpnts/common/card.js
@@ -1,9 +1,10 @@
-export default function Card({ title, value, info, colSpan = 1, rowSpan = 1}) {
+export default function Card({ title, value, valueSize="2xl", bold="font-bold", info, colSpan = 1, rowSpan = 1, children}) {
     return (
         <div className={`bg-white rounded-2xl shadow-sm p-5 flex flex-col justify-between col-span-${colSpan} row-span-${rowSpan}`}>
             <p className="text-gray-400 text-sm">{title}</p>
-            <p className="text-2xl font-bold">{value}</p>
+            <p className={`text-${valueSize} ${bold}`}>{value}</p>
             <p className="text-green-500 text-sm mt-2">{info}</p>
+            {children}
         </div>
     )
 }
--- src/app/u/cmpnts/pages/dashboard.js
+++ src/app/u/cmpnts/pages/dashboard.js
@@ -3,13 +3,13 @@ import Card from "../common/card"
 export default function DashboardMember() {
     return (
         <div className="grid grid-cols-4 auto-rows-[130px] gap-5 w-full">
+            <Card title="Available Balance" value="0" valueSize="4xl" clas info="" rowSpan={2} colSpan={2}><button className="p-2 bg-blue-400 rounded-lg">Withdraw</button></Card>
+            <Card title="Commissions" value="0" info="" colSpan={2}/>
+            <Card title="Total Referred" value="0" info="" colSpan={2}/>
+            <Card title="Referral Link" value="http://localhost:3000/u/profile/pauloreevebuta/MEM-MTE8PA" valueSize="lg" bold="" info="" colSpan={4}/>
             <Card title="" value="" info=""/>
             <Card title="" value="" info=""/>
-            <Card title="" value="" info="" rowSpan={2}/>
-            <Card title="" value="" info="" rowSpan={2}/>
             <Card title="" value="" info=""/>
-            <Card title="" value="" info=""/>
-            <Card title="" value="" info="" colSpan={2} rowSpan={2}/>
             <Card title="" value="" info=""/>
             <Card title="" value="" info=""/>
             <Card title="" value="" info=""/>
