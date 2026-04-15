e1d30f977b83252872532124fa36169a8147d4c7
Paulo <pauloreeve143@gmail.com>
2026-04-15 18:31:23 +0800

    fix: transaction content

--- src/app/components/ui/member/Transactions.js
+++ src/app/components/ui/member/Transactions.js
@@ -15,7 +15,7 @@ export default function Transactions({transactions}) {
 
     return (
         <div>
-            <div className="grid grid-cols-4 shadow-sm p-5 mt-5 rounded-lg bg-white font-semibold">
+            <div className="grid grid-cols-5 shadow-sm p-5 mt-5 rounded-lg bg-white font-semibold">
                 <div>Date</div>
                 <div>Type</div>
                 <div>Amount</div>
@@ -26,11 +26,12 @@ export default function Transactions({transactions}) {
             {transactions.map((transaction, index) => (
                 <div
                     key={index}
-                    className="grid grid-cols-4 shadow-sm p-5 rounded-lg bg-white mt-2"
+                    className="grid grid-cols-5 shadow-sm p-5 rounded-lg bg-white mt-2"
                 >
                     <div>{format(new Date(transaction.created_at), "MMM dd, yyyy")}</div>
                     <div>{transaction.type.charAt(0).toUpperCase() + transaction.type?.slice(1)}</div>
                     <div>₱{transaction.amount}</div>
+                    <div>{transaction.payment_method}</div>
                     <div className="flex items-center justify-between">
                         <span
                             className={
